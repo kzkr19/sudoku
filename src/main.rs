d90d10b1ca7ddb7859fb2aff74c60f3388d523d6
TonyMooori <tony.mooori@gmail.com>
2019-05-10 11:12:47 +0900

    Chap2 implementedd

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate piston;
+extern crate glutin_window;
 
 use piston::window::WindowSettings;
+use glutin_window::GlutinWindow;
+use piston::event_loop::{Events,EventSettings,EventLoop};
 
 fn main() {
     let settings = WindowSettings::new("Sudoku",[512;2])
         .exit_on_esc(true);
+    let mut window : GlutinWindow = settings.build()
+        .expect("Cound not create window");
         
-    println!("{}",settings.get_exit_on_esc());
+    let mut events = Events::new(EventSettings::new().lazy(true));
 }
